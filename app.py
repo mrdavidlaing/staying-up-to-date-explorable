eadb8814bb7d16417c24182f10d9d122fd5f3c2e
David Laing <david@davidlaing.com>
2020-08-30 15:02:04 +0100

    Basic plot of the k8s support escalator - #2

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
 import dash
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SKETCHY])
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+k8s_releases = pd.read_csv("./k8s-releases.csv", parse_dates=['release_date','end_of_support_date'] )
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+fig = px.timeline(k8s_releases, x_start="release_date", x_end="end_of_support_date", y="version")
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+app.layout = dbc.Container(children=[
+    dbc.NavbarSimple(
+        children=[],
+        brand="Staying up-to-date with Kubernetes",
+        brand_href="#",
+        # fixed="top",
+        color="primary",
+        dark=True,
+    ),
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
+    html.Div(
+        [
+            dbc.Row(dbc.Col(html.H1("The support escalator"))),
+            dbc.Row(dbc.Col(html.Div(children='''
+                Descriptive paragraph here
+            '''), width="auto")),
+            dbc.Row(
+                dbc.Col(dcc.Graph(
+                    id='example-graph',
+                    figure=fig
+                ), width="auto")
+            ),
+        ]
     )
 ])
 
