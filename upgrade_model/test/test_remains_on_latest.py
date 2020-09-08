18187904f7dbc8e12cd718216a2b38464ced4c7c
David Laing <david@davidlaing.com>
2020-09-08 11:18:08 +0100

    Fix test name

--- upgrade_model/test/test_remains_on_latest.py
+++ upgrade_model/test/test_remains_on_latest.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from upgrade_model import remain_on_latest
 
-def test_should_generate_a_row_for_every_month():
+def test_should_generate_a_row_for_every_day():
     environment_state = remain_on_latest.compute(start_date='2018-01-01', end_date='2019-01-01')
-    print("\n",environment_state.tail(10))
+    #print("\n",environment_state.tail(10))
     assert len(environment_state)==366
