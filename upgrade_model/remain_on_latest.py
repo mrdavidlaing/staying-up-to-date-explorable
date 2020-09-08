a6a556cddc5d152a7966f8afba6e4f8262fd51c5
David Laing <david@davidlaing.com>
2020-09-08 11:32:27 +0100

    Allow setting environment Id

--- upgrade_model/remain_on_latest.py
+++ upgrade_model/remain_on_latest.py
@@ -10,10 +10,10 @@ def predict_version(for_date):
 
     return k8s_release['version'][0], k8s_release['release_date'][0], k8s_release['end_of_support_date'][0], release_age[0]
 
-def compute(start_date, end_date):
+def compute(id, start_date, end_date):
     months = pd.date_range(start=start_date, end=end_date, freq='D')
     
-    environment_ids = ['simple-env-1']
+    environment_ids = [id]
     environment_state = pd.DataFrame(
         index=pd.MultiIndex.from_product([environment_ids,months],names=['environment_id','at_date'])
         ,data=[]
