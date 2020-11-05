c40efae78d1cd06a87955602094fe41d4024d869
David Laing <david@davidlaing.com>
2020-11-05 13:19:17 +0000

    Visualise multiple upgrade cycles

--- pages/upgrade_cycle.py
+++ pages/upgrade_cycle.py
@@ -226,19 +226,30 @@ def update_output(environment_group_count, environments_per_group, upgrade_failu
     )
 
 #EXPERIMENTAL: This visualisation might be a bit busy
-def generate_upgrade_steps_with_support_elevator(environment_groups, upgrade_failure_percentage, maintenance_window):
-    df_upgrade_steps = upgrade_cycle.compute_next_upgrade_cycle(
-        start_date = datetime.fromisoformat('2020-03-01'),
+def generate_upgrade_steps_with_support_elevator(start_date, upgrade_version_sequence, environment_groups, upgrade_failure_percentage, maintenance_window):
+    df_upgrade_steps = pd.DataFrame()
+    next_start_date = start_date
+    start_version = upgrade_version_sequence[0]
+    end_version = upgrade_version_sequence[-1]
+
+    for current_version, next_version in zip(upgrade_version_sequence, upgrade_version_sequence[1:]):
+        df_next_upgrade_steps = upgrade_cycle.compute_next_upgrade_cycle(
+            start_date = next_start_date,
         environment_groups = environment_groups,
         upgrade_failure_percentage = upgrade_failure_percentage,
         maintenance_window = maintenance_window
     )
+        df_next_upgrade_steps.phase = f"{current_version} -> {next_version}: " + df_next_upgrade_steps.phase
+        df_upgrade_steps = pd.concat([df_upgrade_steps, df_next_upgrade_steps], sort=False)
+        next_start_date = df_upgrade_steps.finish_date.max()
     
     k8s_releases = k8s_releases_loader.load()
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     
-    fig_upgrade_steps = px.timeline(df_upgrade_steps, x_start="start_date", x_end="finish_date", y="phase", color="step")
+    fig_upgrade_steps = px.timeline(
+        df_upgrade_steps, x_start="start_date", x_end="finish_date", y="phase", color="step"
+    )
     fig_support_escalator = px.timeline(k8s_releases, x_start="release_date", x_end="end_of_support_date", y="version", color_discrete_sequence=["LightGray"], opacity=0.5)
     
     fig_upgrade_steps.update_traces(yaxis="y2")
@@ -246,16 +257,27 @@ def generate_upgrade_steps_with_support_elevator(environment_groups, upgrade_fai
     add_weekend_markers(fig, df_upgrade_steps.start_date.min(), df_upgrade_steps.finish_date.max())
     fig.update_layout(fig_upgrade_steps.layout)
     
-    fig.update_yaxes(secondary_y=True, side='left')
-    fig.update_yaxes(secondary_y=False, range=[12,13], side='right', visible=False)
     fig.update_xaxes(range=[
         df_upgrade_steps.start_date.min()  - timedelta(days=7), 
         df_upgrade_steps.finish_date.max() + timedelta(days=7)
     ])
+    #Format the (left) upgrade steps axis
+    fig.update_yaxes(
+        secondary_y=True, side='left', 
+        categoryorder='array', categoryarray=df_upgrade_steps.phase.unique().tolist() 
+    )
+    #Format the (right) support escalator axis
+    fig.update_yaxes(secondary_y=False, side='right', visible=False, range=[
+        k8s_releases.version[k8s_releases.version == start_version ].index[0],
+        k8s_releases.version[k8s_releases.version == end_version].index[0],
+    ])
     
     cycle_start = df_upgrade_steps.start_date.min()
     cycle_end = df_upgrade_steps.finish_date.max()
-    fig.update_layout(title=f"Upgrade cycle: {cycle_start.date()} to {cycle_end.date()} = {(cycle_end - cycle_start).days} days")
+    fig.update_layout(
+        title_text=f"{start_version} -> {end_version}: ({cycle_start.date()} to {cycle_end.date()}) = {(cycle_end - cycle_start).days} days", title_x=0.01,
+        height=700,
+    )
 
     return fig
 
@@ -271,6 +293,8 @@ def generate_upgrade_steps_with_support_elevator(environment_groups, upgrade_fai
 )
 def update_output_with_support_escalator(environment_group_count, environments_per_group, upgrade_failure_percentage, maintenance_window, recalc_counter):
     return generate_upgrade_steps_with_support_elevator(
+        start_date = datetime.fromisoformat('2020-03-01'),
+        upgrade_version_sequence = ['1.15.0','1.16.0','1.17.0','1.18.0'],
         environment_groups=[
             upgrade_cycle.EnvironmentGroup(f'Group {i+1}', [
                 upgrade_cycle.Environment(f'Cluster {j+1}') for j in range(environments_per_group) 
